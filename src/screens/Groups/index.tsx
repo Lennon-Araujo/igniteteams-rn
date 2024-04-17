0e8fc536d1bfe6ec19123fea886f2e60d0dd3070
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-17 11:00:41 -0300

    create ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from '@/components/Highlight';
 import { GroupCard } from '@/components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@/components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
@@ -24,6 +25,12 @@ export function Groups() {
             title={item.item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+        <ListEmpty
+          message='Que tal cadastrar a primeira turma?'
+        />
+      )}
       />
       
     </Container>
