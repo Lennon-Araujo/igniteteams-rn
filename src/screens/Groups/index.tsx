fc104356e39f1062a5ce4e24886c556eaa5e258f
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-17 10:52:47 -0300

    create List with FlatList for GroupCards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,12 @@ import { Header } from '@/components/Header';
 import { Container } from './styles';
 import { Highlight } from '@/components/Highlight';
 import { GroupCard } from '@/components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+
   return (
     <Container>
       <Header />
@@ -12,7 +16,16 @@ export function Groups() {
         subtitle='jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={(item) => (
+          <GroupCard
+            title={item.item}
+          />
+        )}
+      />
+      
     </Container>
   );
 }
