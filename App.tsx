8be953cee093c3d676f392e68d1ac64da8e22202
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-17 09:42:11 -0300

    using theme and create Loading component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import { Groups } from "@/screens/Groups";
 import theme from "@/theme";
+import { Loading } from "@/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme} >
-      <Groups />
+      {
+        fontsLoaded ? <Groups /> : <Loading />
+      }
     </ThemeProvider>
   );
 }
