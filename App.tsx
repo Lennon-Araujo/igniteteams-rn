be978213e6811529b95fc2bad778128a27c3395c
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-04-17 12:57:58 -0300

    initiate to create a new screen NewGroup re-using components

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { Groups } from "@/screens/Groups";
 import theme from "@/theme";
 import { Loading } from "@/components/Loading";
+import { NewGroup } from "@/screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
